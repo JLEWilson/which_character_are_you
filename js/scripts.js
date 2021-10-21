79d9fe0c1ecc3bb27b8d9d312eb1edd2f37fd01b
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-10-21 15:26:28 -0800

    Add showCharacter()
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,29 @@ function add(val1, val2, val3) {
 function showCharacter(total) {
   if (total < 10) {
     //you are pam
+    $("div.output").append("<h3>You are Pam!</h3>");
+    $("div.output").append("<img class='img' src='img/pam.png'>");
+    $("div.output").append("<p>Pam is a level headed person with simpler tastes.</p>");
   } else if( total >= 10 && total < 50){
     //you are jim
+    $("div.output").append("<h3>You are Jim!<h3>");
+    $("div.output").append("<img class='img' src='img/jim.png'>");
+    $("div.output").append("<p>Jim is a funny sportsman that enjoys healthy living.</p>");
   } else if(total >=50 && total <150){
     //you are dwight
+    $("div.output").append("<h3>You are Dwight!</h3>");
+    $("div.output").append("<img class='img' src='img/dwight.png'>");
+    $("div.output").append("<p>Dwight is a follower who also has many surprising skills.</p>");
   } else if(total >=150){
     //you are michael
+    $("div.output").append("<h3>You are Michael!</h3>");
+    $("div.output").append("<img class='img' src='img/michael.png'>");
+    $("div.output").append("<p>Michael is a person that has very little understanding of other peoples feelings or needs.</p>");
   } else{
     alert("You broke it!");
   }
+  $("#button").hide
 }
-/*
-pam < 10  
-jim 10-50
-dwight 50-150
-michael + 150+*/
 
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
@@ -31,3 +39,4 @@ $(document).ready(function() {
     showCharacter(totalValue);
   });
 });
+
